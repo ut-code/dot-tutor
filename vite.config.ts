22e18c7be45c13a2ee7775c2d5be728f919da6e2
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-19 18:37:13 +0900

    Setup PR Preview

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "development" ? "/" : "/learn-braille/",
+  base: process.env.NODE_ENV === "development" ? "/" : process.env.BASE_URL,
 });

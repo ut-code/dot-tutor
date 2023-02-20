c8329186bfe7da27202501d0fd57302353d70aec
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-02-20 17:32:53 +0900

    Add keyboard link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <h1>ホーム</h1>
+      <Link to={"/keyboard"}>点字キーボード</Link>
     </>
   );
 }

40b5118e7432d71eb4e781e27807f81cb2c3bf21
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-02 18:23:46 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_URL =
+  process.env.NODE_ENV === "development" ? "" : process.env.BASE_URL;
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "development" ? "" : process.env.BASE_URL,
-  basePath: process.env.NODE_ENV === "development" ? "" : process.env.BASE_URL,
+  assetPrefix: `${BASE_URL}/`,
+  basePath: BASE_URL,
   images: {
     unoptimized: true,
   },

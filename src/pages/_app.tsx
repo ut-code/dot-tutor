e52f7d46805b73abd1e092cadc25aa2eda319479
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-03 23:07:48 +0900

    Setup _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,66 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import NextLink from "next/link";
+import Image from "next/image";
+import {
+  AppBar,
+  Toolbar,
+  Stack,
+  Typography,
+  Link,
+  IconButton,
+  Box,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>点字学習ソフトウェア</title>
+        <meta name="description" content="点字学習ソフトウェアです。" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <AppBar component="nav">
+        <Toolbar>
+          <Stack direction="row" spacing={2} alignItems="center" flexGrow={1}>
+            <Image
+              src="/logo-black-background.svg"
+              alt="ロゴ"
+              width="150"
+              height="40"
+            />
+            <Typography variant="h5" component="div">
+              点字学習ソフトウェア
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Link
+              href="/"
+              component={NextLink}
+              color="inherit"
+              underline="hover"
+              sx={{ fontSize: "large" }}
+            >
+              Home
+            </Link>
+            <IconButton
+              color="inherit"
+              href="https://github.com/ut-code/learn-braille"
+            >
+              <GitHubIcon fontSize="large" />
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Box m={2}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </Box>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,13 @@
-import Head from "next/head";
-import NextLink from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Stack,
-  Link,
-  IconButton,
-  Box,
   Card,
   CardActionArea,
   CardMedia,
   CardContent,
+  Box,
+  Typography,
   Grid,
 } from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
 
 function NavigationCard(props: {
   title: string;
@@ -46,73 +37,30 @@ function NavigationCard(props: {
 export default function Home(): JSX.Element {
   return (
     <>
-      <Head>
-        <title>点字学習ソフトウェア</title>
-        <meta name="description" content="点字学習ソフトウェアです" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-      <AppBar component="nav">
-        <Toolbar>
-          <Stack direction="row" spacing={2} alignItems="center" flexGrow={1}>
-            <Image
-              src="/logo-black-background.svg"
-              alt="ロゴ"
-              width="150"
-              height="40"
-            />
-            <Typography variant="h5" component="div">
-              点字学習ソフトウェア
-            </Typography>
-          </Stack>
-          <Stack direction="row" spacing={2} alignItems="center">
-            <Link
-              href="/"
-              component={NextLink}
-              color="inherit"
-              underline="hover"
-              sx={{ fontSize: "large" }}
-            >
-              Home
-            </Link>
-            <IconButton
-              color="inherit"
-              href="https://github.com/ut-code/learn-braille"
-            >
-              <GitHubIcon fontSize="large" />
-            </IconButton>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
       <Box m={2}>
-        <main>
-          <Box m={2}>
-            <Typography variant="h5" component="h1">
-              ようこそ
-            </Typography>
-            <Typography variant="body1" component="p">
-              ここでは、チュートリアル形式で点字について学ぶことができます。
-            </Typography>
-          </Box>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <NavigationCard
-                title="点字キーボード"
-                linkUrl="/keyboard"
-                imgPath="/logo.svg"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <NavigationCard
-                title="Web 点字器"
-                linkUrl="/touch"
-                imgPath="/logo.svg"
-              />
-            </Grid>
-          </Grid>
-        </main>
+        <Typography variant="h5" component="h1">
+          ようこそ
+        </Typography>
+        <Typography variant="body1" component="p">
+          ここでは、チュートリアル形式で点字について学ぶことができます。
+        </Typography>
       </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <NavigationCard
+            title="点字キーボード"
+            linkUrl="/keyboard"
+            imgPath="/logo.svg"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <NavigationCard
+            title="Web 点字器"
+            linkUrl="/touch"
+            imgPath="/logo.svg"
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }

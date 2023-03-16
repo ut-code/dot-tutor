a25c455b72b0b70a926c414e2e7c03340e7aed3e
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-16 21:33:18 +0900

    Improve touch design

--- src/pages/touch/index.tsx
+++ src/pages/touch/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { type BrailleChar } from "../../components/brailleDefinitions";
 import translateBraille from "../../components/translateBraille";
 import EdittableBraille from "./EdittableBraille";
+import { Paper, Typography, Divider, Button } from "@mui/material";
 import Layout from "../../components/Layout";
 import Tutorial1 from "./tutorial/tutorial1.mdx";
 import Tutorial2 from "./tutorial/tutorial2.mdx";
@@ -20,28 +21,48 @@ export default function Touch(): JSX.Element {
           { title: "見出し 2", content: <Tutorial2 /> },
         ]}
       >
-        <button
-          type="button"
+        <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
+          <Typography variant="h6" component="h2" color="inherit" p={2}>
+            点字
+          </Typography>
+          <Divider />
+          {brailleStrings.map((brailleChar, i) => (
+            <EdittableBraille
+              key={i}
+              height={"100"}
+              width={"60"}
+              brailleChar={brailleChar}
+              updateBrailleChar={(brailleChar) => {
+                setBrailleStrings(
+                  brailleStrings.map((_, j) => (j === i ? brailleChar : _))
+                );
+              }}
+            />
+          ))}
+        </Paper>
+        <Button
+          variant="contained"
           onClick={() => {
             setHiraganaStrings(translateBraille(brailleStrings));
           }}
         >
           翻訳
-        </button>
-        <p>{hiraganaStrings}</p>
-        {brailleStrings.map((brailleChar, i) => (
-          <EdittableBraille
-            key={i}
-            height={"100"}
-            width={"60"}
-            brailleChar={brailleChar}
-            updateBrailleChar={(brailleChar) => {
-              setBrailleStrings(
-                brailleStrings.map((_, j) => (j === i ? brailleChar : _))
-              );
-            }}
-          />
-        ))}
+        </Button>
+        <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
+          <Typography variant="h6" component="h2" color="inherit" p={2}>
+            墨字
+          </Typography>
+          <Divider />
+          <Typography
+            variant="body1"
+            component="div"
+            color="inherit"
+            p={2}
+            sx={{ minHeight: 100 }}
+          >
+            {hiraganaStrings}
+          </Typography>
+        </Paper>
       </Layout>
     </>
   );

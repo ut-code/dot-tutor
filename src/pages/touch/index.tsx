e2e2b00188fe877eae5d9b68dbf28560f8b60611
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-02 13:42:47 +0900

    Improve directory structure in touch

--- src/pages/touch/index.tsx
+++ src/pages/touch/index.tsx
@@ -1,135 +1,20 @@
-import React, { useState } from "react";
-import { type BrailleChar } from "../../components/brailleDefinitions";
-import translateBraille from "../../components/translateBraille";
-import {
-  judge,
-  makeQuestion,
-  vowelQuestions,
-} from "../../components/questionAndJudge";
-import EdittableBraille from "../../components/EdittableBraille";
-import { Paper, Typography, Divider, Button } from "@mui/material";
+import React from "react";
 import Tutorial from "../../components/Tutorial";
-import Tutorial1 from "./tutorial/tutorial1.mdx";
-import Tutorial2 from "./tutorial/tutorial2.mdx";
+import Tutorial1 from "./tutorial/tutorial1";
+import Tutorial1Dialog1 from "./tutorial/tutorial1/dialog/dialog1.mdx";
+import Tutorial1Dialog2 from "./tutorial/tutorial1/dialog/dialog2.mdx";
 
 export default function Touch(): JSX.Element {
-  const [brailleStrings, setBrailleStrings] = useState<BrailleChar[]>(
-    [...Array(10)].map((_) => "⠀")
-  );
-  const [hiraganaStrings, setHiraganaStrings] = useState<string>("");
-  const [question, setQuestion] = useState<string>("あ"); // 問題
-  const [rightOrWrong, judgeAnswer] = useState<string>(); // 正誤
-
-  function NextQuestion(): JSX.Element {
-    if (rightOrWrong === "正解") {
-      return (
-        <Button
-          variant="contained"
-          onClick={() => {
-            setQuestion(makeQuestion(vowelQuestions));
-            setHiraganaStrings("");
-            judgeAnswer("");
-          }}
-        >
-          次の問題
-        </Button>
-      );
-    } else {
-      return <></>;
-    }
-  }
-
   return (
     <>
       <Tutorial
         tutorialSteps={[
           {
             dialog: [
-              { title: "点字を打ってみよう", content: <Tutorial1 /> },
-              { title: "点字を打ってみよう", content: <Tutorial2 /> },
+              { title: "点字を打ってみよう", content: <Tutorial1Dialog1 /> },
+              { title: "点字を打ってみよう", content: <Tutorial1Dialog2 /> },
             ],
-            content: (
-              <>
-                <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
-                  <Typography variant="h6" component="h2" color="inherit" p={2}>
-                    問題
-                  </Typography>
-                  <Divider />
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    color="inherit"
-                    p={2}
-                    sx={{ minHeight: 100 }}
-                  >
-                    {question}
-                  </Typography>
-                </Paper>
-
-                <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
-                  <Typography variant="h6" component="h2" color="inherit" p={2}>
-                    点字
-                  </Typography>
-                  <Divider />
-                  {brailleStrings.map((brailleChar, i) => (
-                    <EdittableBraille
-                      key={i}
-                      height={"100"}
-                      width={"60"}
-                      brailleChar={brailleChar}
-                      updateBrailleChar={(brailleChar) => {
-                        setBrailleStrings(
-                          brailleStrings.map((_, j) =>
-                            j === i ? brailleChar : _
-                          )
-                        );
-                      }}
-                    />
-                  ))}
-                </Paper>
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    judgeAnswer(judge(brailleStrings, question));
-                    setHiraganaStrings(translateBraille(brailleStrings));
-                  }}
-                >
-                  答え合わせ
-                </Button>
-                <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
-                  <Typography variant="h6" component="h2" color="inherit" p={2}>
-                    墨字
-                  </Typography>
-                  <Divider />
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    color="inherit"
-                    p={2}
-                    sx={{ minHeight: 100 }}
-                  >
-                    {hiraganaStrings}
-                  </Typography>
-                </Paper>
-                <Paper elevation={2} sx={{ mt: 2, mb: 2 }}>
-                  <Typography variant="h6" component="h2" color="inherit" p={2}>
-                    正誤
-                  </Typography>
-                  <Divider />
-                  <Typography
-                    variant="body1"
-                    component="div"
-                    color="inherit"
-                    p={2}
-                    sx={{ minHeight: 100 }}
-                  >
-                    {rightOrWrong}
-                  </Typography>
-                </Paper>
-
-                <NextQuestion />
-              </>
-            ),
+            content: <Tutorial1 />,
           },
         ]}
       />

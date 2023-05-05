a652112d2b377221ccd1a7341b38c8899c1308ff
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-05 11:46:10 +0900

    Add comment to EdittableBraille

--- src/components/EdittableBraille.tsx
+++ src/components/EdittableBraille.tsx
@@ -3,14 +3,23 @@ import { type BrailleState, availableDots } from "../types/brailleDefinitions";
 import { Braille } from "@/models/Braille";
 
 /**
- * component to create touch-to-change Braille
- * @param param0 props
- * @param param0.height height
- * @param param0.width width
- * @param param0.brailleChar character of Braille
- * @param param0.updateBrailleChar function to update the character of braille
- * @param param0.index index of the current character
- * @returns touch-to-change SVG Braille
+ * Component for displaying touch-to-change braille
+ * @param height height of the SVG
+ * @param width width of the SVG
+ * @param braille braille instance to display
+ * @param updateBraille function to update the braille instance when braille has been changed
+ * @returns touch-to-change SVG braille
+ * @example
+ * const [braille, setBraille] = useState<Braille>(new Braille("unicode", "⠀"));
+ *
+ * <EdittableBraille
+ *   height="100"
+ *   width="60"
+ *   braille={braille}
+ *   updateBraille={(braille) => {
+ *     setBraille(braille);
+ *   }}
+ * />
  */
 export default function EdittableBraille({
   height,
@@ -32,6 +41,9 @@ export default function EdittableBraille({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [brailleState]);
 
+  /**
+   * list of x coordinates for each dot
+   */
   const xCoordinateList = {
     Dot1: "40",
     Dot2: "40",
@@ -40,6 +52,9 @@ export default function EdittableBraille({
     Dot5: "90",
     Dot6: "90",
   };
+  /**
+   * list of y coordinates for each dot
+   */
   const yCoordinateList = {
     Dot1: "30",
     Dot2: "75",

1bf60b43da13f7e7378611c18c7202c1647ffbd5
brdgb <104970845+brdgb@users.noreply.github.com>
2023-05-30 12:55:45 +0900

    keyboard practice update

--- src/pages/keyboard/tutorial/tutorial1/index.tsx
+++ src/pages/keyboard/tutorial/tutorial1/index.tsx
@@ -12,16 +12,26 @@ import { BrailleString } from "@/models/Braille";
 import translateBraille from "../../../../utils/translateBraille";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
-export const questionList1: string[] = ["て", "ん", "じ"];
-export const questionList2: string[] = ["あ"];
-export const questionList3: string[] = ["い"];
-export const questionList4: string[] = ["う"];
-export const questionList5: string[] = ["え"];
+export const questionList1: string[][] = [["て"], ["ん"], ["じ"]];
+export const questionList2: string[][] = [
+  ["弟は 算数を 習う", "おとーとわ さんすーを ならう"],
+];
+export const questionList3: string[][] = [
+  ["2023"],
+  ["9と 4分の3番 線", "9と 4ぶんの3ばん せん"],
+];
+export const questionList4: string[][] = [["Tokyo"]];
+export const questionList5: string[][] = [
+  [
+    "今日の 天気は、 雨 のち 曇りです。",
+    "きょーの てんきわ、 あめ のち くもりです。",
+  ],
+];
 
 export default function Tutorial1({
   questionList,
 }: {
-  questionList: string[];
+  questionList: string[][];
 }): JSX.Element {
   const [typedBrailleString, setTypedBrailleString] = useTypedBrailleString();
   const [questionIndex, setQuestionIndex] = useState<number>(0);
@@ -34,7 +44,17 @@ export default function Tutorial1({
     );
   }, [typedBrailleString]);
   useEffect(() => {
-    setGoNextQuestion(translatedBrailleString === questionList[questionIndex]);
+    if (questionList !== undefined && questionIndex < questionList.length) {
+      if (questionList[questionIndex][1] === undefined) {
+        setGoNextQuestion(
+          translatedBrailleString === questionList[questionIndex][0]
+        );
+      } else {
+        setGoNextQuestion(
+          translatedBrailleString === questionList[questionIndex][1]
+        );
+      }
+    }
   }, [translatedBrailleString, questionIndex, questionList]);
   useEffect(() => {
     setQuestionIndex(0);
@@ -51,7 +71,7 @@ export default function Tutorial1({
           {questionList !== undefined &&
             (questionIndex >= questionList.length
               ? "すべての問題を解きました！"
-              : `「${questionList[questionIndex]}」を入力してください。`)}
+              : `「${questionList[questionIndex][0]}」を入力してください。`)}
         </Typography>
       </Paper>
 
@@ -92,6 +112,7 @@ export default function Tutorial1({
             if (questionIndex < questionList.length) {
               setQuestionIndex(questionIndex + 1);
             }
+            setGoNextQuestion(false);
           }
         }}
         disabled={!goNextQuestion}

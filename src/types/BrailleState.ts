b65649c3a56c2f3857fdd08b916b6f62ddfeac8b
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-06-04 19:08:21 +0900

    Revert "Add SixDotBraille types"
    This reverts commit ce003d340d55e83233962dc15482bed3878ece29.

--- src/types/BrailleState.ts
+++ src/types/BrailleState.ts
@@ -30,34 +30,3 @@ type AvailableDot = keyof BrailleState;
 export const availableDots = Object.keys(
   defaultBrailleStateValue
 ) as AvailableDot[];
-
-/**
- * The default state of 6-dot braille
- */
-export const defaultSixDotBrailleStateValue = {
-  Dot1: false,
-  Dot2: false,
-  Dot3: false,
-  Dot4: false,
-  Dot5: false,
-  Dot6: false,
-};
-
-/**
- * The type of the state of 6-dot braille
- */
-export type SixDotBrailleState = typeof defaultSixDotBrailleStateValue;
-
-/**
- * The type of the available dots of 6-dot braille
- */
-type SixDotBrailleAvailableDot = keyof SixDotBrailleState;
-
-/**
- * The array of the available dots of 6-dot braille
- * @example
- * const sixDotBrailleAvailableDots = ["Dot1", "Dot2", "Dot3", "Dot4", "Dot5", "Dot6"];
- */
-export const sixDotBrailleAvailableDots = Object.keys(
-  defaultSixDotBrailleStateValue
-) as SixDotBrailleAvailableDot[];

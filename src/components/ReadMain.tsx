079e429f2f2bfc6de6197a7c802b070ffe60b572
sawa3030 <esawada2003@gmail.com>
2023-06-27 15:35:32 +0900

    update ReadMain and added translateSumiji

--- src/components/ReadMain.tsx
+++ src/components/ReadMain.tsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import translateBraille from "@/utils/translateBraille";
+import translateSumiji from "@/utils/translateSumiji";
 import { judge, makeQuestion } from "@/components/questionAndJudge";
 import EdittableBraille from "@/components/EdittableBraille";
-import { Paper, Typography, Divider, Button } from "@mui/material";
+import {
+  Paper,
+  Typography,
+  Divider,
+  Button,
+  Box,
+  TextField,
+} from "@mui/material";
 import { SixDotBraille } from "@/models/BrailleCharacter";
 import { SixDotBrailleString } from "@/models/BrailleString";
 
@@ -11,7 +19,34 @@ export default function ReadMain({
 }: {
   typeOfQuestions: string[];
 }): JSX.Element {
-  let brailleStrings = makeQuestion(typeOfQuestions);
+  const [question, setQuestion] = useState<string>(
+    makeQuestion(typeOfQuestions)
+  ); // 問題
+  const [questionInBraille, setQuestionInBraille] = useState<SixDotBraille[]>(
+    translateSumiji(question)
+  ); // 問題
+  const [typedAns, setTypedAns] = useState<string>("");
+  const [rightOrWrong, judgeAnswer] = useState<string>(); // 正誤
+
+  function NextQuestion(): JSX.Element {
+    if (rightOrWrong === "正解") {
+      return (
+        <Button
+          variant="contained"
+          onClick={() => {
+            setQuestion(makeQuestion(typeOfQuestions));
+            setQuestionInBraille(translateSumiji(question));
+            judgeAnswer("");
+          }}
+        >
+          次の問題
+        </Button>
+      );
+    } else {
+      return <></>;
+    }
+  }
+
   return (
     <>
       <Paper elevation={2} sx={{ my: 2 }}>
@@ -19,15 +54,46 @@ export default function ReadMain({
           問題
         </Typography>
         <Divider />
-        {brailleStrings}を点字にしよう
+        {questionInBraille.map((brailleChar, i) => (
+          <EdittableBraille
+            key={i}
+            height={"100"}
+            width={"60"}
+            braille={brailleChar}
+            updateBraille={(braille) => {}}
+          />
+        ))}
       </Paper>
-
       <Paper elevation={2} sx={{ my: 2 }}>
         <Typography variant="h6" component="h2" color="inherit" p={2}>
           解答
         </Typography>
         <Divider />
+        <Box sx={{ minHeight: 100 }} p={2}>
+          <TextField
+            variant="outlined"
+            value={typedAns}
+            style={{ width: "100%" }}
+            onChange={(e) => {
+              setTypedAns(e.target.value);
+            }}
+          />
+        </Box>
       </Paper>
+      <Button
+        variant="contained"
+        onClick={() => {
+          judgeAnswer(
+            judge(
+              new SixDotBrailleString("braille array", questionInBraille),
+              typedAns
+            )
+          );
+        }}
+      >
+        答え合わせ
+      </Button>
+      <NextQuestion />
     </>
   );
 }
--- src/pages/touch/tutorial/tutorial6/index.tsx
+++ src/pages/touch/tutorial/tutorial6/index.tsx
@@ -1,6 +1,8 @@
 import ReadMain from "@/components/ReadMain";
+import { SixDotBraille } from "@/models/BrailleCharacter";
 
-export const vowelQuestions = ["⠁", "⠃"];
+// export const vowelQuestions: SixDotBraille[] = ["⠁","⠃"];
+export const vowelQuestions = ["あ", "い"];
 
 export default function Practice1(): JSX.Element {
   return <ReadMain typeOfQuestions={vowelQuestions} />;

a60e4de67760b31f9c963fd56cad7e8dc718a9cd
ts5-25 <104970913+ts5-25@users.noreply.github.com>
2023-07-11 19:52:26 +0900

    複数ページにわたる場合も対応

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,7 +18,7 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import { useTheme } from "@mui/material/styles";
 import { API_ENDPOINT } from "./commons/config";
-import { unicodeToBes } from './modules/unicodeToBes';
+import { unicodeToBes } from "./modules/unicodeToBes";
 
 function App() {
   const [displaySourceText, setDisplaySourceText] = useState(
@@ -167,14 +167,20 @@ function App() {
                 Copy
               </Button>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Button onClick={() => {const buffer = unicodeToBes(targetText.replace(/\n/g, "\\n"));
-              const blob = new Blob([buffer], { type: "text/plain" });
-              const url = URL.createObjectURL(blob);
-              const a = document.createElement("a");
-              a.href = url;
-              a.download = "output.BES";
-              a.click();}}>Output</Button>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button
+                onClick={() => {
+                  const buffer = unicodeToBes(targetText.replace(/\n/g, "\\n"));
+                  const blob = new Blob([buffer], { type: "text/plain" });
+                  const url = URL.createObjectURL(blob);
+                  const a = document.createElement("a");
+                  a.href = url;
+                  a.download = "output.BES";
+                  a.click();
+                }}
+              >
+                Output
+              </Button>
             </div>
           </Box>
         </Box>
--- web/src/modules/unicodeToBes.ts
+++ web/src/modules/unicodeToBes.ts
@@ -1,76 +1,80 @@
 import { PagesRounded } from "@mui/icons-material";
 
-export function 
-unicodeToBes(unicode: string): Uint8Array {
-    const output = [0x25, 0x42, 0x45, 0x54, 0x34, 0x30, 0x30, 0x25, 0x32, 0x30, 0x32, 0x33, 0x2F, 0x30, 0x35, 0x2F, 0x31, 0x36, 0x20, 0x20, 0x45, 0x35, 0x33, 0x33, 0x32, 0x32, 0x20, 0x20, 0x20, 0x20, 0x20, 0x30]
-    for (var i = 0; i < (512 - 32); i++) {
-        output.push(0x20);
-    }
-    const semiheader = [0xff, 0xff, 0xff, 0x54];
-    for (var i = 0; i < 5; i++) {
-        output.push(semiheader[i]);
-    }
-    for (var i = 0; i < (511 - 4); i++) {
-        output.push(0x20);
-    }
+export function unicodeToBes(unicode: string): Uint8Array {
+  const output = [
+    0x25, 0x42, 0x45, 0x54, 0x34, 0x30, 0x30, 0x25, 0x32, 0x30, 0x32, 0x33,
+    0x2f, 0x30, 0x35, 0x2f, 0x31, 0x36, 0x20, 0x20, 0x45, 0x35, 0x33, 0x33,
+    0x32, 0x32, 0x20, 0x20, 0x20, 0x20, 0x20, 0x30,
+  ];
+  for (var i = 0; i < 512 - 32; i++) {
+    output.push(0x20);
+  }
+  const semiheader = [0xff, 0xff, 0xff, 0x54];
+  for (var i = 0; i < 5; i++) {
+    output.push(semiheader[i]);
+  }
+  for (var i = 0; i < 511 - 4; i++) {
+    output.push(0x20);
+  }
 
-    var header = [0xff, 0x00, 0x00, 0xfd];
-    // 2つ目,3つ目はBESファイルの文字数を表す
+  var header = [0xff, 0x00, 0x00, 0xfd];
+  // 2つ目,3つ目はBESファイルの文字数を表す
 
-    for (var i = 0; i < 4; i++) {
-        output.push(header[i]);
-        console.log(output.length);
-    }
-    var all_letters = 0;
-    for (var i = 0; i < 27; i++) {
+  for (var i = 0; i < 4; i++) {
+    output.push(header[i]);
+    console.log(output.length);
+  }
+  var all_letters = 0;
+  for (var i = 0; i < 27; i++) {
+    output.push(0xa0);
+    all_letters += 1;
+  }
+  output.push(0xfe);
+  var letters = 0;
+  var lines = 0;
+  var pages = 0;
+  for (const char of unicode) {
+    if (lines == 21) {
+      /*for (var i = 0; i < 27; i++) {
         output.push(0xa0);
         all_letters += 1;
+      }
+      output.push(0xfe);*/
+      lines = 0;
+      pages += 1;
     }
-    output.push(0xfe);
-    var letters = 0;
-    var lines = 0;
-    var pages = 0;
-    for (const char of unicode) {
-      const codePoint = char.codePointAt(0);
-      console.log(char + " letters: " + letters);
-      if (codePoint != null) {
-        if (codePoint >= 0x2800 && codePoint <= 0x28ff) {
-          output.push(codePoint - 0x2800 + 0xa0);
-          letters += 1;
-          all_letters += 1;
-        }
-      }
-      if (char == "n") {
-        output.push(0x0d);
-        output.push(0xfe);
-        lines += 1;
-        letters = 0;
-      }
-      if (letters == 32) {
-        output.push(0xfe);
-        lines += 1;
-        letters = 0;
-        console.log("fe");
-      }
-      if (lines == 21) {
-        for (var i = 0; i < 27; i++) {
-          output.push(0xa0);
-        }
-        output.push(0xfe);
-        lines = 0;
-        pages += 1;
+    const codePoint = char.codePointAt(0);
+    console.log(char + " letters: " + letters);
+    if (codePoint != null) {
+      if (codePoint >= 0x2800 && codePoint <= 0x28ff) {
+        output.push(codePoint - 0x2800 + 0xa0);
+        letters += 1;
+        all_letters += 1;
       }
     }
-    output[31] = 0x30 + pages;
-    for (var i = 0; i + lines < 21; i++) {
+    if (char == "n") {
+      output.push(0x0d);
       output.push(0xfe);
+      lines += 1;
+      letters = 0;
     }
-    output.push(0xff);
-    
-    // 文字数を代入
-    var letters_num = 0x1a + all_letters;
-    output[1025] = letters_num % 255 + 12;
-    output[1026] = Math.floor(letters_num / 255);
-
-    return new Uint8Array(output);
+    if (letters == 32) {
+      output.push(0xfe);
+      lines += 1;
+      letters = 0;
+      console.log("fe");
+    }
   }
+  output[31] = 0x30 + pages;
+  for (var i = 0; i + lines < 21; i++) {
+    output.push(0xfe);
+  }
+  output.push(0xff);
+
+  // 文字数を代入
+  var letters_num = 0x1a + all_letters;
+  output[1025] = (letters_num % 255) + 12;
+  output[1026] = Math.floor(letters_num / 255);
+
+  return new Uint8Array(output);
+}

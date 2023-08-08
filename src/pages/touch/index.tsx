b5ca03653d3f509aa81e79814e7c80d3db18e9e9
sawa3030 <esawada2003@gmail.com>
2023-08-08 09:44:55 +0900

    added PracticeRead and tutorial for read

--- src/pages/touch/index.tsx
+++ src/pages/touch/index.tsx
@@ -12,7 +12,12 @@ import Tutorial1Dialog3 from "./tutorial/tutorial2/dialog/3_write_gojyuon.mdx";
 import Tutorial1Dialog4 from "./tutorial/tutorial3/dialog/4_write_dakuon_handakuon.mdx";
 import Tutorial1Dialog5 from "./tutorial/tutorial4/dialog/5_write_contraction.mdx";
 import Tutorial1Dialog6 from "./tutorial/tutorial5/dialog/6_write_special_hiragana.mdx";
-import Tutorial1Dialog7 from "./tutorial/tutorial6/dialog/7_read_vowel.mdx";
+import Tutorial1Dialog7 from "./tutorial/tutorial6/dialog/1_read.mdx";
+import Tutorial1Dialog8 from "./tutorial/tutorial6/dialog/2_read_vowel.mdx";
+import Tutorial1Dialog9 from "./tutorial/tutorial6/dialog/3_read_gojyuon.mdx";
+import Tutorial1Dialog10 from "./tutorial/tutorial6/dialog/4_read_dakuon_handakuon.mdx";
+import Tutorial1Dialog11 from "./tutorial/tutorial6/dialog/5_read_contraction.mdx";
+import Tutorial1Dialog12 from "./tutorial/tutorial6/dialog/6_read_special_hiragana.mdx";
 
 export default function Touch(): JSX.Element {
   return (
@@ -65,9 +70,17 @@ export default function Touch(): JSX.Element {
         },
         {
           dialog: [
+            { title: "点字を読んでみよう", content: <Tutorial1Dialog7 /> },
+            { title: "母音を読んでみよう", content: <Tutorial1Dialog8 /> },
+            { title: "五十音を読んでみよう", content: <Tutorial1Dialog9 /> },
             {
-              title: "点字を読んでみよう",
-              content: <Tutorial1Dialog7 />,
+              title: "濁音、半濁音を読んでみよう",
+              content: <Tutorial1Dialog10 />,
+            },
+            { title: "拗音を読んでみよう", content: <Tutorial1Dialog11 /> },
+            {
+              title: "その他のひらがなを学んで、色々な文字を読んでみよう",
+              content: <Tutorial1Dialog12 />,
             },
           ],
           content: <Practice6 />,

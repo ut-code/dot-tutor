9bd77a7b20ac418fb22ade96f26db9dcc5403c72
sawa3030 <esawada2003@gmail.com>
2023-08-14 19:31:55 +0900

    separated section

---
+++ src/pages/read/tutorial/tutorial1/index.tsx
@@ -0,0 +1,16 @@
+import TouchMain from "@/components/TouchMain";
+
+export const vowelQuestions = [
+  "あい",
+  "あう",
+  "あお",
+  "いう",
+  "いえ",
+  "おい",
+  "おう",
+  "おお",
+];
+
+export default function Practice1(): JSX.Element {
+  return <TouchMain typeOfQuestions={vowelQuestions} brailleDotCount={6} />;
+}

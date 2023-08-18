ed044e007a284d545df521f1be255b1a1549c401
random776 <kazuki.tdn@gmail.com>
2023-08-18 18:50:10 +0900

    update 部首 explanation

---
+++ src/pages/kanji/tutorial/tutorial9/index.tsx
@@ -0,0 +1,10 @@
+import TouchMain from "@/components/TouchMain";
+import { Card, Typography } from "@mui/material";
+
+export const advancedkanjiQuestions = ["続編", "談話", "住宅", "安定"]; // 濁音、半濁音が含まれる言葉
+
+export default function Practice4(): JSX.Element {
+  return (
+    <TouchMain typeOfQuestions={advancedkanjiQuestions} brailleDotCount={8} />
+  );
+}

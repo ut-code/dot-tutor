b3fd93e4315714719b4abce90ea920b6a1ceeeda
random776 <kazuki.tdn@gmail.com>
2023-08-22 15:39:41 +0900

    add tutorials to kanji

--- src/pages/kanji/index.tsx
+++ src/pages/kanji/index.tsx
@@ -3,9 +3,13 @@ import Tutorial from "@/components/Tutorial";
 import Practice6 from "./tutorial/tutorial6";
 import Practice7 from "./tutorial/tutorial7";
 import Practice8 from "./tutorial/tutorial8";
+import Practice9 from "./tutorial/tutorial9";
+import Practice10 from "./tutorial/tutorial10";
 import Tutorial1Dialog7 from "./tutorial/tutorial6/dialog/7_write_kanji.mdx";
 import Tutorial1Dialog8 from "./tutorial/tutorial7/dialog/8_advanced_kanji_1.mdx";
 import Tutorial1Dialog9 from "./tutorial/tutorial8/dialog/9_advanced_kanji_2.mdx";
+import Tutorial1Dialog10 from "./tutorial/tutorial9/dialog/10_advanced_kanji_3.mdx";
+import Tutorial1Dialog11 from "./tutorial/tutorial10/dialog/11_advanced_kanji_4.mdx";
 
 export default function Touch(): JSX.Element {
   return (
@@ -41,6 +45,28 @@ export default function Touch(): JSX.Element {
             },
           ],
           content: <Practice8 />,
+          hasHintHiraganaTable: false,
+          hasHintAlphabetTable: false,
+        },
+        {
+          dialog: [
+            {
+              title: "構造を意識して漢点字を学習しよう③",
+              content: <Tutorial1Dialog10 />,
+            },
+          ],
+          content: <Practice9 />,
+          hasHintHiraganaTable: false,
+          hasHintAlphabetTable: false,
+        },
+        {
+          dialog: [
+            {
+              title: "構造を意識して漢点字を学習しよう④",
+              content: <Tutorial1Dialog11 />,
+            },
+          ],
+          content: <Practice10 />,
           hasHintHiraganaTable: false,
           hasHintAlphabetTable: false,
         },

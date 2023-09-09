144611730c0050f2987f570cbafe165e756cdce4
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-09-09 18:22:16 +0900

    Remove `AbstractBrailleCharacter` class

--- learn/src/models/BrailleCharacter/BrailleCharacter.ts
+++ learn/src/models/BrailleCharacter/BrailleCharacter.ts
@@ -1,4 +1,4 @@
-import AbstractBrailleCharacter from "./AbstractBrailleCharacter";
+import BrailleBase from "../BrailleBase/BrailleBase";
 import {
   CharacterType,
   DotCountType,
@@ -7,15 +7,16 @@ import {
   SixDotBrailleCharacterType,
   SixDotBrailleDotCountType,
 } from "../BrailleBase/types";
-import Validator from "../BrailleBase/validations/Validator";
 
 /**
  * A class representing the information of a braille character.
  */
-export default class BrailleCharacter<
+export default abstract class BrailleCharacter<
   Character extends CharacterType,
   DotCount extends DotCountType,
-> extends AbstractBrailleCharacter<Character, DotCount> {
+> {
+  private readonly braille: BrailleBase<Character, DotCount>;
+
   /**
    * Constructs a new instance with the given braille character.
    * @param character a braille character
@@ -30,8 +31,31 @@ export default class BrailleCharacter<
     dotCount: EightDotBrailleDotCountType,
   );
   constructor(character: Character, dotCount: DotCount) {
-    Validator.validateDotCount(dotCount);
-    Validator.validateBrailleCharacter(character, dotCount);
-    super(character, dotCount);
+    this.braille = new BrailleBase(character, dotCount);
+  }
+
+  /**
+   * Gets the Unicode of the braille character.
+   * @returns the Unicode of the braille character
+   */
+  getUnicode(): Character {
+    return this.braille.getUnicode();
+  }
+
+  /**
+   * Gets the number of dots based on the type of braille.
+   * @returns the number of dots based on the type of braille
+   */
+  getDotCount(): DotCount {
+    return this.braille.getDotCount();
+  }
+
+  /**
+   * Checks if the braille character is equal to the other.
+   * @param other the other braille character
+   * @returns true if the braille character is equal to the other, false otherwise
+   */
+  equals(other: this): boolean {
+    return this.braille.equals(other.braille);
   }
 }

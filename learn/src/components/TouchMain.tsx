3e1fcfbb56e9e8dc9caa400d95528cfb9e70087c
sawa3030 <esawada2003@gmail.com>
2023-09-24 14:57:45 +0900

    draft of ui

--- learn/src/components/TouchMain.tsx
+++ learn/src/components/TouchMain.tsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import translateBraille from "@/utils/translateBraille";
 import { judge, eightJudge, makeQuestion } from "@/components/QuestionAndJudge";
 import EdittableBraille from "@/components/EdittableBraille";
-import { Paper, Typography, Divider, Button, Stack, Box } from "@mui/material";
+import {
+  Paper,
+  Typography,
+  Divider,
+  Button,
+  Stack,
+  Box,
+  Container,
+} from "@mui/material";
 import { BrailleString } from "@/models/BrailleString";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import RefreshIcon from "@mui/icons-material/Refresh";
@@ -25,6 +33,7 @@ export default function TouchMain({
   const [hiraganaStrings, setHiraganaStrings] = useState<string>("");
   const [question, setQuestion] = useState<string>(typeOfQuestions[0]); // 問題
   const [rightOrWrong, judgeAnswer] = useState<boolean>(false); // 正誤
+  const [afterJudgeAnswer, setAfterJudgeAnswer] = useState<boolean>(false); // 正誤
 
   useEffect(() => {
     setHiraganaStrings(
@@ -49,119 +58,127 @@ export default function TouchMain({
 
   return (
     <>
-      <Paper elevation={2} sx={{ my: 2 }}>
-        <Typography variant="h6" component="h2" color="inherit" p={2}>
-          問題
-        </Typography>
-        <Divider />
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          pt: 8,
+          pb: 6,
+        }}
+      >
         <Typography
-          variant="body1"
-          component="div"
-          color="inherit"
-          p={2}
-          sx={{ minHeight: 100 }}
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          paragraph
         >
           「{question}」を点字に直してください。
         </Typography>
-      </Paper>
-
-      <Paper elevation={2} sx={{ my: 2 }}>
-        <Stack direction="row" p={2} spacing={2}>
-          <Typography variant="h6" component="h2" color="inherit">
-            点字
-          </Typography>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              setBrailleStrings(
-                new BrailleString(
-                  "unicode",
-                  [...Array(10)].map((_) => "⠀").join(""),
-                  brailleDotCount,
-                ),
-              );
-            }}
-            startIcon={<RefreshIcon />}
-          >
-            リセット
-          </Button>
-        </Stack>
-        <Divider />
-        {brailleStrings.brailleArray.map((brailleChar, i) => (
-          <EdittableBraille
-            key={i}
-            height="100"
-            width="60"
-            braille={brailleChar}
-            setBraille={(braille) => {
-              setBrailleStrings(
-                new BrailleString(
-                  "braille array",
-                  brailleStrings.brailleArray.map((_, j) =>
-                    j === i ? braille : _,
+        <Typography>
+          {brailleStrings.brailleArray.map((brailleChar, i) => (
+            <EdittableBraille
+              key={i}
+              height="100"
+              width="60"
+              braille={brailleChar}
+              setBraille={(braille) => {
+                setBrailleStrings(
+                  new BrailleString(
+                    "braille array",
+                    brailleStrings.brailleArray.map((_, j) =>
+                      j === i ? braille : _,
+                    ),
+                    brailleDotCount,
                   ),
-                  brailleDotCount,
-                ),
-              );
-            }}
-          />
-        ))}
-      </Paper>
-      <Paper elevation={2} sx={{ my: 2 }}>
-        <Typography variant="h6" component="h2" color="inherit" p={2}>
-          墨字
+                );
+              }}
+            />
+          ))}
         </Typography>
-        <Divider />
-        <Typography
-          variant="body1"
-          component="div"
-          color="inherit"
-          p={2}
-          sx={{ minHeight: 100 }}
-        >
-          {hiraganaStrings}
-        </Typography>
-      </Paper>
-      <Paper elevation={2} sx={{ my: 2 }}>
-        <Typography variant="h6" component="h2" color="inherit" p={2}>
-          正誤
-        </Typography>
-        <Divider />
-        <Typography
-          variant="body1"
-          component="div"
-          color="inherit"
-          p={2}
-          sx={{ minHeight: 100 }}
-        >
-          {rightOrWrong ? (
-            <Box display="flex">
-              正解 <CheckCircleOutlineIcon color="success" />
-            </Box>
-          ) : (
-            "不正解"
-          )}
-        </Typography>
-      </Paper>
+      </Box>
 
-      {rightOrWrong && (
-        <Button
-          variant="contained"
-          onClick={() => {
-            setQuestion(makeQuestion(typeOfQuestions));
-            setBrailleStrings(
-              new BrailleString(
-                "unicode",
-                [...Array(10)].map((_) => "⠀").join(""),
-                brailleDotCount,
-              ),
-            );
-            judgeAnswer(false);
-          }}
-        >
-          次の問題
-        </Button>
-      )}
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        {afterJudgeAnswer === true ? (
+          rightOrWrong === true ? (
+            <Button
+              variant="contained"
+              onClick={() => {
+                setQuestion(makeQuestion(typeOfQuestions));
+                setBrailleStrings(
+                  new BrailleString(
+                    "unicode",
+                    [...Array(10)].map((_) => "⠀").join(""),
+                    brailleDotCount,
+                  ),
+                );
+                judgeAnswer(false);
+                setAfterJudgeAnswer(false);
+              }}
+            >
+              次の問題へ
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              onClick={() => {
+                setQuestion(makeQuestion(typeOfQuestions));
+                setBrailleStrings(
+                  new BrailleString(
+                    "unicode",
+                    [...Array(10)].map((_) => "⠀").join(""),
+                    brailleDotCount,
+                  ),
+                );
+                judgeAnswer(false);
+                setAfterJudgeAnswer(false);
+              }}
+            >
+              次の問題へ
+            </Button>
+          )
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              judgeAnswer(
+                brailleDotCount === 6
+                  ? judge(brailleStrings, question)
+                  : eightJudge(brailleStrings, question),
+              );
+              setAfterJudgeAnswer(true);
+            }}
+          >
+            答え合わせ
+          </Button>
+        )}
+      </Box>
+
+      {afterJudgeAnswer === true ? (
+        rightOrWrong === true ? (
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            正解
+          </Typography>
+        ) : (
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            不正解
+          </Typography>
+        )
+      ) : null}
     </>
   );
 }

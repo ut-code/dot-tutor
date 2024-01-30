8c60ae92c5ca2c5ce576eb22c6fefe6d047ec415
brdgb <bridgebhot@gmail.com>
2024-01-30 14:57:58 +0900

    add minimum page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,46 @@
+import { Box, CardMedia, Grid, Link, Typography } from "@mui/material";
+
 function App() {
-  return <>Dot Tutorのウェブサイトです。</>;
+  return (
+    <>
+      <Box sx={{ width: "85%", margin: "auto" }}>
+        <Grid container alignItems="center" justifyContent="center">
+          <Grid item xs={12} lg={6}>
+            <Box>
+              <Typography variant="h1" align="center">
+                Dot Tutor
+              </Typography>
+
+              <Box display={"flex"} alignItems="center" justifyContent="center">
+                <Box display={"flex"}>
+                  <Typography variant="h5" mr={5}>
+                    点字を身近に。
+                  </Typography>
+                  <Typography variant="h5" mr={1}>
+                    by
+                  </Typography>
+                  <img src="/logo.svg" height="38em"></img>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <CardMedia component="img" image="/image_pic.svg"></CardMedia>
+          </Grid>
+        </Grid>
+        <Box>
+          <Link href="https://translate.dot-tutor.com/">
+            Dot Tutor Translate (翻訳サイト) はこちら
+          </Link>
+        </Box>
+        <Box>
+          <Link href="https://learn.dot-tutor.com/">
+            Dot Tutor Learn (学習サイト) はこちら
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default App;
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontWeight: 600,
+    },
+    fontFamily: [
+      "ui-sans-serif",
+      "system-ui",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      '"Noto Sans"',
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Noto Color Emoji"',
+    ].join(","),
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
